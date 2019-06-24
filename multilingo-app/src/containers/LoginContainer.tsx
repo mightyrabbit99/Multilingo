53a6524def605077cea5ec205de4bdd53f6637c7
wahken <t.1999ken@gmail.com>
2019-06-24 18:58:46 +0800

    loginpage link completed

--- multilingo-app/src/containers/LoginContainer.tsx
+++ multilingo-app/src/containers/LoginContainer.tsx
@@ -6,6 +6,7 @@ import { bindActionCreators, Dispatch } from 'redux';
 import Login, { LoginPageDispatchProps, LoginPageStateProps } from '../components/LoginPage';
 import { IState } from '../reducers/states';
 import { Action as ReduxAction } from 'redux';
+import { push } from 'connected-react-router';
 
 
 const mapStateToProps: MapStateToProps<LoginPageStateProps, {}, IState> = state => ({
@@ -15,8 +16,6 @@ const mapStateToProps: MapStateToProps<LoginPageStateProps, {}, IState> = state 
 const mapDispatchToProps: MapDispatchToProps<LoginPageDispatchProps, {}> = (dispatch: Dispatch<ReduxAction>) =>
   bindActionCreators<any, any>(
     {
-      handleLogin: () => {}
-
     },
     dispatch
 	);
