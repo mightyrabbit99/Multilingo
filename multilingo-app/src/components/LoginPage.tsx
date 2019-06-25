59997bd8c2bec3a209558ba648d9e9e7bc4523d6
HonHaoChen <haochenhon@hotmail.com>
2019-06-25 09:30:55 +0800

    loginpagechanges

--- multilingo-app/src/components/LoginPage.tsx
+++ multilingo-app/src/components/LoginPage.tsx
@@ -1,16 +1,26 @@
 import * as React from "react";
-import { Button, Form } from "semantic-ui-react";
+import {
+  Button,
+  Form,
+  Segment,
+  Grid,
+  Divider,
+  Header,
+  Icon,
+  Search
+} from "semantic-ui-react";
 
-import { RouteComponentProps } from 'react-router';
+import { RouteComponentProps } from "react-router";
 
-export interface LoginPageProps extends LoginPageStateProps, LoginPageDispatchProps, RouteComponentProps<{}> {}
+export interface LoginPageProps
+  extends LoginPageStateProps,
+    LoginPageDispatchProps,
+    RouteComponentProps<{}> {}
 
-export interface LoginPageStateProps {
-
-}
+export interface LoginPageStateProps {}
 
 export interface LoginPageDispatchProps {
-	handleLogin: () => void;
+  handleLogin: () => void;
 }
 
 export interface LoginPageState {}
@@ -20,17 +30,39 @@ class LoginPage extends React.Component<LoginPageProps, LoginPageState> {
 
   render() {
     return (
-      <Form>
-        <Form.Field>
-          <label>Email</label>
-          <input placeholder="your email..." />
-        </Form.Field>
-        <Form.Field>
-          <label>Password</label>
-          <input placeholder="your password..." />
-        </Form.Field>
-        <Button type="submit">Submit</Button>
-      </Form>
+      <div>
+        <Segment
+          placeholder
+          color="green"
+          style={{ margin: "10vh 30vw 0vh 30vw" }}
+        >
+          <Grid rows={2} stackable textAlign="center">
+            <Grid.Column verticalAlign="middle">
+              <Grid.Row>
+                <Form>
+                  <Form.Field>
+                    <label>Email</label>
+                    <input placeholder="your email" />
+                  </Form.Field>
+                  <Form.Field>
+                    <label>Password</label>
+                    <input placeholder="your password" />
+                  </Form.Field>
+                  <Button type="submit">Register</Button>
+                </Form>
+              </Grid.Row>
+              <Divider horizontal>Or</Divider>
+
+              <Grid.Row>
+                <Button color="olive">
+                  <Icon name="google" style={{ marginRight: "0.8em" }} />
+                  Sign Up with Google{" "}
+                </Button>
+              </Grid.Row>
+            </Grid.Column>
+          </Grid>
+        </Segment>
+      </div>
     );
   }
 }
